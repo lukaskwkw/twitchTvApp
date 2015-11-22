249852425559041155093b8b77c9ed05df09177e
lukaskwkw <lukaskwkw@gmail.com>
2015-11-22 12:14:09 +0100

    new layout added glyphicons and positioning

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,11 +3,13 @@ var streamsArr =
 "habathcx","medrybw","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"]
 
 var query = "https://api.twitch.tv/kraken/streams/"
-
+	var totalUsers=0;
+	var onlineUsers=0;
+	var offlineUsers=0;
 $(document).ready(function(){
+
 	for (var i= 0; i<streamsArr.length; i++)
 		$.getJSON(query+streamsArr[i],populateLists);
-$(function () { $("[data-toggle='tooltip']").tooltip(); });
 });
 
 function populateLists(json)
@@ -17,44 +19,50 @@ function populateLists(json)
 	var tabOffline = $('.js-tab-offline');
 	var html='';
 	console.log(json);
-		// $(tabAll).append('<ul class="user list-group">');
 		if (json.stream!==null)
 		{	
-
+			totalUsers++;
+			onlineUsers++;
 			var logo = getProperty(json.stream.channel,'logo');
+			if (logo=="")
+				logo = "http://placehold.it/64x64";
 			var display_name = getProperty(json.stream.channel,'display_name');
 			var status = getProperty(json.stream.channel,'status');
-
-			//html='<ul class="user list-group">';
-			html+='<li class="list-group-item list-group-item-success" id="'+display_name+'" ><img class="user__img img img-circle" src="'
-			+logo+'"></img>'+
+			var url = getProperty(json.stream.channel,'url');
+			html="";
+			html+='<a href="'+url+'" target="_blank"><li class="user list-group-item" id="'+display_name+'"><img class="user__img img img-circle" src="'
+			+logo+'"/>'+
 			'<span class="user__name">'
-			+display_name+'  </span>'+
-			'<a href="#" class="user_info label label-info" data-toggle="tooltip" title="'
-			+status+'">info</a>'+
-			'</div>'+
-			'</li>';
-			// $('#'+display_name).effect("bounce");
+			+display_name+'</span><i class="user__status text-success glyphicon glyphicon-ok-circle"></i>'+
+			'<span class="text-info user_info glyphicon glyphicon-exclamation-sign" data-toggle="tooltip" title="'
+			+status+'"/>'+
+			'</li></span>';
 			$(tabAll).children().append(html);
 			$(tabOnline).children().append(html);
+			$('.js-all-count').html(totalUsers);
+			$('.js-online-count').html(onlineUsers);
 		}
 		else
 		{
-			// console.log(json._links.channel);
 			$.getJSON(json._links.channel+"?callback=?", function(json2){
-				// console.log(json2);
+				totalUsers++;
+				offlineUsers++;
 				var logo =  getProperty(json2,'logo');
+				if (logo=="")
+				logo = "http://placehold.it/64x64";
 				var display_name = getProperty(json2,'display_name');
-				// html='<ul class="user list-group">';
-				html+='<li class="list-group-item list-group-item-danger" id="'+display_name+'"><img class="user__img img img-circle" src="'
+				var url = getProperty(json2,'url');
+				html="";
+				html+='<a href="'+url+'" target="_blank"><li class="user list-group-item" id="'+display_name+'"><img class="user__img img img-circle" src="'
 				+logo+'"></img>'+
 				'<span class="user__name">'
-				+display_name+'</span>'+
-				'</div>'+
-				'</li>';
-				// html+='</ul>';
+				+display_name+'</span><i class="user__status text-danger glyphicon glyphicon-minus-sign"></i>'+
+				'</li></a>';
 				$(tabAll).children().append(html);
-				$(tabOffline).children().append(html)		
+				$(tabOffline).children().append(html);
+				$('.js-all-count').html(totalUsers);
+				$('.js-offline-count').html(offlineUsers);
+		
 			})
 		}		
 $('[data-toggle="tooltip"]').tooltip({
@@ -72,25 +80,5 @@ $('[data-toggle="tooltip"]').tooltip({
 		}
 		else
 		{
-			console.log('nic');
 			return '';}
-	}
-	function populateOffline(argument) {
-	// body...
-}
-
-
-/*
-
-if (stream!==null)
-
-preview.small
-channel.status
-
-else
-
-.getJSON(_links.channel)
-display_name
-logo
-
-*/
+		}
